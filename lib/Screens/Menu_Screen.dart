fe321bae5a5583dfdcb08597924616facf658be8
arora <aroramadhav65@gmail.com>
2022-08-06 19:02:55 +0530

    Update

--- lib/Screens/Menu_Screen.dart
+++ lib/Screens/Menu_Screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:threed_sidebar_menu/Models/Menu_Item.dart';
 
 class MenuItems {
-  static const payment = MenuItem();
+  static const payment = MenuItem('Payment');
 }
 
 class MenuScreen extends StatelessWidget {
