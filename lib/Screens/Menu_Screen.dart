1b2c15e8efb878e99dcb2a32a166aacd56ae3dbb
arora <aroramadhav65@gmail.com>
2022-08-06 19:12:55 +0530

    Update

--- lib/Screens/Menu_Screen.dart
+++ lib/Screens/Menu_Screen.dart
@@ -10,6 +10,10 @@ class MenuItems {
     'Payment',
     Icons.payment,
   );
+  static const payment = MenuItem(
+    'Payment',
+    Icons.payment,
+  );
 }
 
 class MenuScreen extends StatelessWidget {
