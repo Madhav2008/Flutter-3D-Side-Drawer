a16509772af310057aa433d9c3c8e6a68493a164
arora <aroramadhav65@gmail.com>
2022-08-06 16:50:26 +0530

    Update

--- lib/Screens/Menu_Screen.dart
+++ lib/Screens/Menu_Screen.dart
@@ -5,6 +5,6 @@ class MenuScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: ,);
+    return Scaffold(backgroundColor: Colors.indigo,);
   }
 }
