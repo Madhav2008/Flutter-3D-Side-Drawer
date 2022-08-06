ecf99aaa979beab9658361be4589b6f1dedbd816
arora <aroramadhav65@gmail.com>
2022-08-06 16:51:48 +0530

    Update

--- lib/Screens/Payment_Screen.dart
+++ lib/Screens/Payment_Screen.dart
@@ -6,7 +6,7 @@ class PaymentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.indigo,
+      appBar: ,
     );
   }
 }
