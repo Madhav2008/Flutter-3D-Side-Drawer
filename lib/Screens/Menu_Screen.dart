8a090691102242e327f308fe93ca17d4c724c72b
arora <aroramadhav65@gmail.com>
2022-08-06 19:40:26 +0530

    Update

--- lib/Screens/Menu_Screen.dart
+++ lib/Screens/Menu_Screen.dart
@@ -49,7 +49,7 @@ class MenuScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Spacer(),
-            ...MenuItems.all.map(buildMenUItem).toList(),
+            ...MenuItems.all.map(buildMenuItem).toList(),
             Spacer(
               flex: 2,
             ),
