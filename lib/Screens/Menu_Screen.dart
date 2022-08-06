3a3ec1f12288a2dab8c58f170d25b527bb98134d
arora <aroramadhav65@gmail.com>
2022-08-06 19:03:20 +0530

    Update

--- lib/Screens/Menu_Screen.dart
+++ lib/Screens/Menu_Screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:threed_sidebar_menu/Models/Menu_Item.dart';
+import 'package:threed_sidebar_menu/Models/Menu_Item.dart';
 
 class MenuItems {
   static const payment = MenuItem('Payment');
