1084360483801ab268060c3859d2f332b991d11e
arora <aroramadhav65@gmail.com>
2022-08-06 19:49:32 +0530

    Update

--- lib/Screens/Menu_Screen.dart
+++ lib/Screens/Menu_Screen.dart
@@ -3,41 +3,7 @@ import 'package:threed_sidebar_menu/Models/Menu_Item.dart';
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({Key? key}) : super(key: key);
-class MenuItems {
-  static const payment = MenuItem(
-    'Payment',
-    Icons.payment,
-  );
-  static const promos = MenuItem(
-    'Promos',
-    Icons.card_giftcard,
-  );
-  static const notifications = MenuItem(
-    'Notifications',
-    Icons.notifications,
-  );
-  static const help = MenuItem(
-    'Help',
-    Icons.help,
-  );
-  static const aboutUs = MenuItem(
-    'About Us',
-    Icons.info_outline,
-  );
-  static const rateUs = MenuItem(
-    'Rate Us',
-    Icons.star_border,
-  );
 
-  static const all = <MenuItem>[
-    payment,
-    promos,
-    notifications,
-    help,
-    aboutUs,
-    rateUs,
-  ];
-}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
