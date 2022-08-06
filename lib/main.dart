017678e515ad499d4887b0e93e8c1b41442246b5
arora <aroramadhav65@gmail.com>
2022-08-06 17:53:23 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      child: MaterialApp(
+      body: MaterialApp(
         title: 'Flutter Demo',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
