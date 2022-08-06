210635145179bc03f0be559e51ab30244953b8d5
arora <aroramadhav65@gmail.com>
2022-08-06 16:49:43 +0530

    Update

--- lib/Screens/Menu_Screen.dart
+++ lib/Screens/Menu_Screen.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/material.dart';class MenuScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+
+class MenuScreen extends StatelessWidget {
   const MenuScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    
-  }
+  Widget build(BuildContext context) {}
 }
