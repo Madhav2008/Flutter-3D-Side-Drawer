f41ac5b7c42ee0d8c6ca76214329985e4d1ad0b8
arora <aroramadhav65@gmail.com>
2022-08-06 17:01:21 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
     );
   }
 }
