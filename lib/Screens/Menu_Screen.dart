470d23bc275cadd4122de37896e9dc67fb6aaa78
arora <aroramadhav65@gmail.com>
2022-08-06 16:50:01 +0530

    Update

--- lib/Screens/Menu_Screen.dart
+++ lib/Screens/Menu_Screen.dart
@@ -4,5 +4,5 @@ class MenuScreen extends StatelessWidget {
   const MenuScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {return}
+  Widget build(BuildContext context) {return Scaffold()}
 }
