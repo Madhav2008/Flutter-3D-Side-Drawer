3db32746276e28bf92e9ab6b8a32ea0afc9e4ff0
arora <aroramadhav65@gmail.com>
2022-08-06 16:58:32 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -36,11 +36,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+ 
 
   @override
   Widget build(BuildContext context) {
