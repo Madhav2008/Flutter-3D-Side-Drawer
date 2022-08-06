d41c542a75d613063586c4aee3e6659dfe3237d9
arora <aroramadhav65@gmail.com>
2022-08-06 16:52:26 +0530

    Update

--- lib/Screens/Payment_Screen.dart
+++ lib/Screens/Payment_Screen.dart
@@ -7,7 +7,7 @@ class PaymentScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.blue,title: ,
       ),
     );
   }
