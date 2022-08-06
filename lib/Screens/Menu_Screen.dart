18ec31613d7183a24291b24125bde00337c687b2
arora <aroramadhav65@gmail.com>
2022-08-06 19:48:51 +0530

    Update

--- lib/Screens/Menu_Screen.dart
+++ lib/Screens/Menu_Screen.dart
@@ -1,42 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:threed_sidebar_menu/Models/Menu_Item.dart';
 
-class MenuItems {
-  static const payment = MenuItem(
-    'Payment',
-    Icons.payment,
-  );
-  static const promos = MenuItem(
-    'Promos',
-    Icons.card_giftcard,
-  );
-  static const notifications = MenuItem(
-    'Notifications',
-    Icons.notifications,
-  );
-  static const help = MenuItem(
-    'Help',
-    Icons.help,
-  );
-  static const aboutUs = MenuItem(
-    'About Us',
-    Icons.info_outline,
-  );
-  static const rateUs = MenuItem(
-    'Rate Us',
-    Icons.star_border,
-  );
-
-  static const all = <MenuItem>[
-    payment,
-    promos,
-    notifications,
-    help,
-    aboutUs,
-    rateUs,
-  ];
-}
-
 class MenuScreen extends StatelessWidget {
   const MenuScreen({Key? key}) : super(key: key);
 
