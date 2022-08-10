4948d8f724472d62f81c678e595421174745c204
arora <aroramadhav65@gmail.com>
2022-08-10 16:33:31 +0530

    Update

--- lib/Screens/Menu_Screen.dart
+++ lib/Screens/Menu_Screen.dart
@@ -74,7 +74,7 @@ class MenuScreen extends StatelessWidget {
                   color: Colors.white,
                 ),
               ),
-              onTap: () {},
+              onTap: () {selected = '1';},
             ),
             ListTile(
               minLeadingWidth: 20,
