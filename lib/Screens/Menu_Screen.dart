2dcdf09dfb16c73cdbe653ab7852b3dcef2a5441
arora <aroramadhav65@gmail.com>
2022-08-10 16:34:29 +0530

    Update

--- lib/Screens/Menu_Screen.dart
+++ lib/Screens/Menu_Screen.dart
@@ -107,7 +107,7 @@ class MenuScreen extends StatelessWidget {
                 ),
               ),
               onTap: () {
-                selected = '6';print(object)
+                selected = '6';print(selected)
               },
             ),
             Spacer(
