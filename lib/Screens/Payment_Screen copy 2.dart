53cf4ccab7e46f636283c243dc48a98682c82b7a
arora <aroramadhav65@gmail.com>
2022-08-10 17:03:57 +0530

    Update

---
+++ lib/Screens/Payment_Screen copy 2.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+
+class PaymentScreen extends StatelessWidget {
+  const PaymentScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Payment'),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () => ZoomDrawer.of(context)!.toggle(),
+          icon: Icon(Icons.menu),
+        ),
+      ),
+    );
+  }
+}
