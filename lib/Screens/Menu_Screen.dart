b757eb9743bd3cbf83fcd1a87e54ed3b46e427be
arora <aroramadhav65@gmail.com>
2022-08-10 16:25:43 +0530

    Update

--- lib/Screens/Menu_Screen.dart
+++ lib/Screens/Menu_Screen.dart
@@ -16,7 +16,7 @@ class MenuScreen extends StatelessWidget {
             ListTile(
               minLeadingWidth: 20,
               leading: Icon(
-                Icons.card_giftcard,
+                Icons.payment,
                 color: Colors.white,
               ),
               title: Text(
