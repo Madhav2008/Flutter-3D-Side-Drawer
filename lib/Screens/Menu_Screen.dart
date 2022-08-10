c57f5d6f785f3c6c9f2a635a59524421a9bade73
arora <aroramadhav65@gmail.com>
2022-08-10 17:01:59 +0530

    Update

--- lib/Screens/Menu_Screen.dart
+++ lib/Screens/Menu_Screen.dart
@@ -41,7 +41,7 @@ class MenuScreen extends StatelessWidget {
                   color: Colors.white,
                 ),
               ),
-              onTap: () {},
+              onTap: () {ZoomDrawer.of(context)!.toggle();},
             ),
             ListTile(
               minLeadingWidth: 20,
