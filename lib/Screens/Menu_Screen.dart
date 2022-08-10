8b48510bb2abab4db511c1e7f57699c497592fdb
arora <aroramadhav65@gmail.com>
2022-08-10 16:21:39 +0530

    Update

--- lib/Screens/Menu_Screen.dart
+++ lib/Screens/Menu_Screen.dart
@@ -55,6 +55,20 @@ class MenuScreen extends StatelessWidget {
               ),
               onTap: () {},
             ),
+            ListTile(
+              minLeadingWidth: 20,
+              leading: Icon(
+                Icons.payment,
+                color: Colors.white,
+              ),
+              title: Text(
+                'Payment',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              onTap: () {},
+            ),
             Spacer(
               flex: 2,
             ),
